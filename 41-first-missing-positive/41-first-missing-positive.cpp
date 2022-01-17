149dcad41c85437e0e48d03c0caa86969b6c802c
Maximizer03 <97230497+Maximizer03@users.noreply.github.com>
2022-01-17 17:38:55 +0530

    Time: 128 ms (98.71%), Space: 86.4 MB (19.40%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
-     int firstMissingPositive(vector<int>& nums) {
-          // try to put the number i at index i-1 leaving -ve and nos >n
-         // dry run for [3,4,7,1]
-          int n = nums.size();
-          for (int i = 0; i < n; i++) {
-               while (1) {
-                    if (nums[i] <= 0 || nums[i] > n) {
-                         break;
-                    }
-                    int cur = nums[i];
-                    int pos = cur - 1;
-                    if (nums[pos] != cur) {
-                         swap(nums[pos], nums[i]);
-                    }
-                    else {
-                         break;
-                    }
-               }
-          }
-          for (int i = 0; i < n; i++) {
-               if (nums[i] != i + 1) {
-                    return i + 1;
-               }
-          }
-          return n + 1;
-     }
+    int firstMissingPositive(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>a(n+5,0);
+        for(auto x:nums){
+            if(x<=n && x>0){
+                a[x]++;
+            }
+        }
+        for(int i=1;i<=n+5;i++){
+            if(a[i]==0){
+                return i;
+            }
+        }
+        return -1;
+    }
 };
