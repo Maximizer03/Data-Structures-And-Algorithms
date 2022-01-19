821507acdf7f4e29c22f7c570ce19d796dd13807
Maximizer03 <97230497+Maximizer03@users.noreply.github.com>
2022-01-19 21:27:42 +0530

    Time: 15 ms (24.85%), Space: 10.4 MB (99.87%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-     int minSubArrayLen(int s, vector<int>& nums) {
-          int n = nums.size();
-          int mx = n + 1,i = 0;
-          for (int j = 0; j < n; j++) {
-               s -= nums[j];
-               while (s <= 0) {
-                    int len = (j - i + 1);
-                    mx = min(mx, len);
-                    s += nums[i];
-                    i++;
-               }
-          }
-          return mx % (n + 1);
-     }
+    int minSubArrayLen(int s, vector<int>& nums) {
+        int n=nums.size();
+        int i=0,mn=n+1;
+        for(int j=0;j<n;j++){
+            s-=nums[j];
+            while(s<=0){
+                mn=min(mn,j-i+1);
+                s+=nums[i];
+                i++;
+            }
+        }
+        return mn%(n+1);
+    }
 };
