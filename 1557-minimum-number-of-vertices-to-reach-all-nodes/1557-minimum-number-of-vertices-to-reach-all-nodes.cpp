8594199fa1849d79bef7eb19b1d41627f55b7a0a
Maximizer03 <97230497+Maximizer03@users.noreply.github.com>
2022-01-19 17:08:58 +0530

    Time: 280 ms (73.08%), Space: 106.8 MB (28.92%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,45 +1,14 @@
 class Solution {
 public:
-     void dfs(int v, int p, vector<int>&vis, vector<vector<int>>&g) {
-          vis[v] = 1;
-          for (auto u : g[v]) {
-               if (u != p && vis[u] == 0) {
-                    dfs(u, v, vis, g);
-               }
-          }
-     }
      vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-          int m = edges.size();
-          vector<int>order, indegree(n, 0), vis(n, 0);
-          vector<vector<int>>g(n);
+          vector<int>indegree(n, 0);
           for (auto x : edges) {
-               // u->v
-               int u = x[0], v = x[1];
-               g[u].push_back(v);
-               indegree[v]++;
-          }
-          queue<int>q;
-          for (int i = 0; i < n; i++) {
-               if (indegree[i] == 0) {
-                    q.push(i);
-               }
-          }
-          while (!q.empty()) {
-               int v = q.front();
-               q.pop();
-               order.push_back(v);
-               for (auto u : g[v]) {
-                    indegree[u]--;
-                    if (indegree[u] == 0) {
-                         q.push(u);
-                    }
-               }
+               indegree[x[1]]++;
           }
           vector<int>ans;
-          for (auto v : order) {
-               if (!vis[v]) {
-                    ans.push_back(v);
-                    dfs(v, -1, vis, g);
+          for (int i = 0; i < n; i++) {
+               if (indegree[i] == 0) {
+                    ans.push_back(i);
                }
           }
           return ans;
