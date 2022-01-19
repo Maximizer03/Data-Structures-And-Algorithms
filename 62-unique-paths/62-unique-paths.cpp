6e0bf73618549b5212336084d75fca4a1fc7e1e2
Maximizer03 <97230497+Maximizer03@users.noreply.github.com>
2022-01-19 16:22:47 +0530

    Time: 0 ms (100.00%), Space: 6 MB (68.70%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
-    int uniquePaths(int n, int m) {
-        vector<vector<int>>dp(n,vector<int>(m,0));
-        dp[0][0]=1;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i-1>=0){
-                    dp[i][j]+=dp[i-1][j];
-                }
-                if(j-1>=0){
-                    dp[i][j]+=dp[i][j-1];
-                }
-            }
-        }
-        return dp[n-1][m-1];
-    }
+     int uniquePaths(int n, int m) {
+          long long ans = 1;
+          for (int i = m + n - 2, j = 1; i >= max(n,m); i--, j++) {
+               ans = (ans * i) / j;
+          }
+          return ans;
+     }
 };
