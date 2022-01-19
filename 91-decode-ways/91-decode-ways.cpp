2649e0a2a01c2c4c2c5b9a46d0e046db4f6ddd6d
Maximizer03 <97230497+Maximizer03@users.noreply.github.com>
2022-01-19 15:46:10 +0530

    Time: 11 ms (6.89%), Space: 7.6 MB (13.08%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,28 +1,29 @@
 class Solution {
 private:
-    vector<int>dp;
+     vector<int>dp;
 public:
-    int calc(int i, string s){
-        int n=s.size();
-        if(i==n){
-            return 1;
-        }
-        if(s[i]=='0'){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int ans= calc(i+1,s);
-        if(i+1<n){
-            if(s[i]=='1' || s[i]=='2'&& s[i+1]<='6'){
-                ans+=calc(i+2,s);
-            }
-        }
-        return dp[i]=ans;
-    }
-    int numDecodings(string s) {
-        dp.resize(s.size()+1,-1);
-        return calc(0,s);
-    }
+     // dp[i]=> number of decode ways when string starts from i
+     int calc(int i, string s) {
+          int n = s.size();
+          if (i == n) {
+               return 1;
+          }
+          if (s[i] == '0') {
+               return 0;
+          }
+          if (dp[i] != -1) {
+               return dp[i];
+          }
+          int ans = calc(i + 1, s);
+          if (i + 1 < n) {
+               if (s[i] == '1' || s[i] == '2' && s[i + 1] <= '6') {
+                    ans += calc(i + 2, s);
+               }
+          }
+          return dp[i] = ans;
+     }
+     int numDecodings(string s) {
+          dp.resize(s.size() + 1, -1);
+          return calc(0, s);
+     }
 };
