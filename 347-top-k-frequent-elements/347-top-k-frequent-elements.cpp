3bc78b03ddfd0a0dc0afa81867f858cd8ae97db3
Maximizer03 <97230497+Maximizer03@users.noreply.github.com>
2022-01-19 23:32:41 +0530

    Time: 27 ms (24.45%), Space: 15.5 MB (5.69%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    struct cmp{
-        bool operator()(const pair<int,int>&a, const pair<int,int>&b){
-            return a.first>b.first;
-        }
-    };
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int>m;
+        // Bucket Sort Superb Technique
+        unordered_map<int,int>cnt;
         for(auto x:nums){
-            m[x]++;
+            cnt[x]++;
         }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,cmp>q;
-        for(auto x:m){
-            pair<int,int>p= {x.second,x.first};
-            q.push(p);
-            if(q.size()>k){
-                q.pop();
+        vector<vector<int>>bucket(nums.size()+1);
+        for(auto x:cnt){
+            bucket[x.second].push_back(x.first);
+        }
+        reverse(begin(bucket),end(bucket));
+        vector<int>res;
+        for(auto x:bucket){
+            for(auto y:x){
+                res.push_back(y);
+                if(res.size()==k){
+                    return res;
+                }
             }
         }
-        vector<int>ans;
-        while(!q.empty()){
-            ans.push_back(q.top().second);
-            q.pop();
-        }
-        return ans;
+        return {};
     }
 };
