0c2e286f434fce360a6bc1f791d9e6ffb88ab001
Maximizer03 <97230497+Maximizer03@users.noreply.github.com>
2022-01-19 21:32:47 +0530

    Time: 162 ms (60.43%), Space: 67.6 MB (93.08%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-     int numberOfSubarrays(vector<int>& nums, int k) {
-          int n = nums.size();
-          vector<int>sums(n + 1, 0);
-          for (int i = 0; i < n; i++) {
-               nums[i] = nums[i] % 2;
-               sums[i + 1] = sums[i] + nums[i];
-          }
-          int ans = 0;
-          for (int i = 1; i <= n; i++) {
-               int x = sums[i - 1] + k;
-               int cnt = upper_bound(sums.begin(), sums.end(), x) - lower_bound(sums.begin(), sums.end(), x);
-               ans += cnt;
-          }
-          return ans;
-     }
+    // calc(array,k)=> number of subarrays with sum atmost k
+    int calc(vector<int>&a, int k){
+        if(k<0){
+            return 0;
+        }
+        int n=a.size();
+        int i=0,ans=0;
+        for(int j=0;j<n;j++){
+            k-=a[j];
+            while(k<0){
+                k+=a[i];
+                i++;
+            }
+            // number of subarrays ending at j and starting from [i....j]
+            ans+=(j-i+1);
+        }
+        return ans;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            nums[i]=nums[i]%2;
+        }
+        return calc(nums,k)-calc(nums,k-1);
+    }
 };
