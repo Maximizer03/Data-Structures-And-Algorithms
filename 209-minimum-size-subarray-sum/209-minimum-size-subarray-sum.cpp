ed3cb46e361eebece282e5008f956ee541691126
Maximizer03 <97230497+Maximizer03@users.noreply.github.com>
2022-01-19 19:25:24 +0530

    Time: 17 ms (18.19%), Space: 10.6 MB (58.61%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-     int minSubArrayLen(int target, vector<int>& nums) {
+     int minSubArrayLen(int s, vector<int>& nums) {
           int n = nums.size();
-          vector<long long>sums(n + 1, 0);
-          for (int i = 1; i <= n; i++) {
-               sums[i] = sums[i - 1] + nums[i - 1];
-          }
-          if (sums[n] < target) {
-               return 0;
-          }
-          int mx = n;
-          for (int i = 1; i <= n; i++) {
-               int l = lower_bound(sums.begin(), sums.end(), target + sums[i - 1]) - sums.begin();
-               if (l == n + 1) {
-                    l--;
-               }
-               long long total = sums[l] - sums[i - 1];
-               if (total >= target) {
-                    mx = min(mx, l - i + 1);
+          int mx = n + 1,i = 0;
+          for (int j = 0; j < n; j++) {
+               s -= nums[j];
+               while (s <= 0) {
+                    int len = (j - i + 1);
+                    mx = min(mx, len);
+                    s += nums[i];
+                    i++;
                }
           }
-          return mx;
+          return mx % (n + 1);
      }
 };
