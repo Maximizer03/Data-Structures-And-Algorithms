e3425459570993df97b9c1d3c50aa68eaee025a5
Maximizer03 <97230497+Maximizer03@users.noreply.github.com>
2022-01-20 19:08:53 +0530

    Time: 837 ms (10.75%), Space: 142.4 MB (25.26%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,30 +1,28 @@
 class Solution {
 private:
-    vector<vector<int>>g;
-    vector<int>tm;
+     vector<vector<int>>g;
+     vector<int>tm;
 public:
-    int ans;
-    void dfs(int v, int p,  int t){
-        ans=max(ans,t);
-        for(auto u:g[v]){
-            if(u!=p){
-                dfs(u,v,t+tm[v]);
-            }
-        }
-    }
-    int numOfMinutes(int n, int root, vector<int>& manager, vector<int>& informTime) {
-        g=vector<vector<int>>(n);
-        tm=informTime;
-        for(int i=0;i<n;i++){
-            if(manager[i]!=-1){
-                int u=i;
-                int v= manager[i];
-                g[u].push_back(v);
-                g[v].push_back(u);
-            }
-        }
-        ans=0;
-        dfs(root,-1,0);
-        return ans;
-    }
+     int ans;
+     void dfs(int v, int p,  int t) {
+          ans = max(ans, t);
+          for (auto u : g[v]) {
+               if (u != p) {
+                    dfs(u, v, t + tm[v]);
+               }
+          }
+     }
+     int numOfMinutes(int n, int root, vector<int>& m, vector<int>& informTime) {
+          g = vector<vector<int>>(n);
+          tm = informTime;
+          for (int i = 0; i < n; i++) {
+               if (m[i] != -1) {
+                    g[m[i]].push_back(i);
+                    g[i].push_back(m[i]);
+               }
+          }
+          ans = 0;
+          dfs(root, -1, 0);
+          return ans;
+     }
 };
