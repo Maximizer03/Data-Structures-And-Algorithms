1ab2a778a10e02a5e3ef6fc220f2f405344b6b68
Maximizer03 <97230497+Maximizer03@users.noreply.github.com>
2022-01-21 22:48:40 +0530

    Time: 3 ms (41.61%), Space: 5.9 MB (25.45%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,20 +1,7 @@
 class Solution {
 public:
      bool isPowerOfFour(int n) {
-          if (n <= 0) {
-               return 0;
-          }
-          while (1) {
-               if (n == 1) {
-                    break;
-               }
-               if (n % 4 == 0) {
-                    n = n / 4;
-               }
-               else {
-                    return 0;
-               }
-          }
-          return 1;
+         
+          return ( n>0 && (n & (n - 1)) == 0 && (n - 1) % 3 == 0);
      }
 };
