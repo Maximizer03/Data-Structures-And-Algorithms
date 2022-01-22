07c365e3ce4c80fd6221a6540af6c8fdb9a2e0a3
Maximizer03 <97230497+Maximizer03@users.noreply.github.com>
2022-01-22 19:56:26 +0530

    Time: 40 ms (79.66%), Space: 14.6 MB (84.16%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
-     struct cmp {
-          bool operator()(const pair<int, int>&a, const pair<int, int>&b) {
-               return a.first < b.first;
-          }
-     };
-     int minFallingPathSum(vector<vector<int>>& matrix) {
-          int n = matrix.size();
-          if (n == 1) {
-               return matrix[0][0];
-          }
-          priority_queue<pair<int, int>, vector<pair<int, int>>, cmp>q;
+     vector<int> getSmallestTwo(vector<int>&v) {
+          int n = v.size();
+          vector<int>a(2, -1);
           for (int i = 0; i < n; i++) {
-               q.push({matrix[0][i], i});
-               if (q.size() > 2) {
-                    q.pop();
+               if (a[0] == -1 || v[i] < v[a[0]]) {
+                    a[1] = a[0];
+                    a[0] = i;
                }
+               else if (a[1] == -1 || v[i] < v[a[1]]) {
+                    a[1] = i;
+               }
+          }
+          return a;
+     }
+     int minFallingPathSum(vector<vector<int>>& A) {
+          int n = A.size();
+          if (n == 1) {
+               return A[0][0];
           }
           for (int i = 1; i < n; i++) {
-               priority_queue<pair<int, int>, vector<pair<int, int>>, cmp>k, r = q;
-               int mn1 = r.top().first, idx1 = r.top().second;
-               r.pop();
-               int mn2 = r.top().first, idx2 = r.top().second;
+               auto a = getSmallestTwo(A[i - 1]);
                for (int j = 0; j < n; j++) {
-                    if (j == idx2) {
-                         k.push({mn1 + matrix[i][j], j});
-
+                    if (a[0] == j) {
+                         A[i][j] += A[i - 1][a[1]];
                     }
                     else {
-                         k.push({mn2 + matrix[i][j], j});
-                    }
-                    if (k.size() > 2) {
-                         k.pop();
+                         A[i][j] += A[i - 1][a[0]];
                     }
                }
-               q = k;
           }
-          q.pop();
-          return q.top().first;
+          return *min_element(A[n - 1].begin(), A[n - 1].end());
      }
 };
