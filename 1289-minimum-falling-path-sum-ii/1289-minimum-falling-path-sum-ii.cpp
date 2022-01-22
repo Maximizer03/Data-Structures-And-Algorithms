387d66fc496688b037678a6d6bcd7a656604c7fd
Maximizer03 <97230497+Maximizer03@users.noreply.github.com>
2022-01-22 19:02:28 +0530

    Time: 792 ms (14.28%), Space: 15.8 MB (43.32%) - LeetHub

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+          vector<vector<int>>dp(n, vector<int>(n, 1e9));
+          for (int i = 0; i < n; i++) {
+               dp[0][i] = matrix[0][i];
+          }
+          for (int i = 1; i < n; i++) {
+               for (int j = 0; j < n; j++) {
+                    for (int k = 0; k <= n-1; k++) {
+                         if (k!=j) {
+                              dp[i][j] = min(dp[i][j], matrix[i][j] + dp[i - 1][k]);
+                         }
+                    }
+               }
+          }
+          int mn = 1e9;
+          for (int i = 0; i < n; i++) {
+               mn = min(mn, dp[n - 1][i]);
+          }
+          return mn;
+    }
+};
