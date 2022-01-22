55721a51f7ed2c47eeb0d3e8fefcdba5dbe8c396
Maximizer03 <97230497+Maximizer03@users.noreply.github.com>
2022-01-22 19:37:13 +0530

    Time: 59 ms (69.10%), Space: 14.8 MB (54.19%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
      struct cmp {
-     bool operator()(const pair<int, int>&a, const pair<int, int>&b) {
-          return a.first < b.first;
-     }
-};
-int minFallingPathSum(vector<vector<int>>& matrix) {
-     int n = matrix.size();
-     if (n == 1) {
-          return matrix[0][0];
-     }
-     priority_queue<pair<int, int>, vector<pair<int, int>>, cmp>q;
-     for (int i = 0; i < n; i++) {
-          q.push({matrix[0][i], i});
-          if (q.size() > 2) {
-               q.pop();
+          bool operator()(const pair<int, int>&a, const pair<int, int>&b) {
+               return a.first < b.first;
           }
-     }
-
-     for (int i = 1; i < n; i++) {
-          priority_queue<pair<int, int>, vector<pair<int, int>>, cmp>k, r = q;
-          int mn1 = r.top().first, idx1 = r.top().second;
-          r.pop();
-          int mn2 = r.top().first, idx2 = r.top().second;
-          cout << mn1 << " " << mn2 << endl;
-          for (int j = 0; j < n; j++) {
-               if (j == idx2) {
-                    k.push({mn1 + matrix[i][j], j});
-
-               }
-               else {
-                    k.push({mn2 + matrix[i][j], j});
-               }
-               if (k.size() > 2) {
-                    k.pop();
+     };
+     int minFallingPathSum(vector<vector<int>>& matrix) {
+          int n = matrix.size();
+          if (n == 1) {
+               return matrix[0][0];
+          }
+          priority_queue<pair<int, int>, vector<pair<int, int>>, cmp>q;
+          for (int i = 0; i < n; i++) {
+               q.push({matrix[0][i], i});
+               if (q.size() > 2) {
+                    q.pop();
                }
           }
-          q = k;
+          for (int i = 1; i < n; i++) {
+               priority_queue<pair<int, int>, vector<pair<int, int>>, cmp>k, r = q;
+               int mn1 = r.top().first, idx1 = r.top().second;
+               r.pop();
+               int mn2 = r.top().first, idx2 = r.top().second;
+               for (int j = 0; j < n; j++) {
+                    if (j == idx2) {
+                         k.push({mn1 + matrix[i][j], j});
+
+                    }
+                    else {
+                         k.push({mn2 + matrix[i][j], j});
+                    }
+                    if (k.size() > 2) {
+                         k.pop();
+                    }
+               }
+               q = k;
+          }
+          q.pop();
+          return q.top().first;
      }
-     q.pop();
-     return q.top().first;
-}
 };
