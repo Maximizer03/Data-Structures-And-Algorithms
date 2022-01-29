fbca51635b7ac1ab6c4074a0f5dc6c638e472a20
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-01-29 16:26:18 +0530

    Time: 108 ms (86.65%), Space: 39.9 MB (71.92%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,38 +1,44 @@
 class Solution {
 public:
 	int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-		vector<vector<int>>dp = shortestDistance(times, n);
-		k--;
-		int maxTime = 0;
-		for (int i = 0; i < n; i++) {
-			if (dp[k][i] == INT_MAX) {
+		vector<vector<pair<int, int>>> g = buildGraph(times, n);
+		vector<int> dist = shortestDistance(g, n, k);
+		int max_time = 0;
+		for (int i = 1; i <= n; i++) {
+			if (dist[i] == INT_MAX) {
 				return -1;
 			}
-			maxTime = max(maxTime, dp[k][i]);
+			max_time = max(max_time, dist[i]);
 		}
-		return maxTime;
+		return max_time;
 	}
 private:
-	vector<vector<int>> shortestDistance(vector<vector<int>>&times, int n) {
-		vector<vector<int>>dist(n, vector<int>(n, INT_MAX));
-		for (int i = 0; i < n; i++) {
-			dist[i][i] = 0;
-		}
+	vector<vector<pair<int, int>>> buildGraph(vector<vector<int>>& times, int n) {
+		vector<vector<pair<int, int>>> graph(n + 1);
 		for (auto &edge : times) {
-			int start = edge[0] - 1;
-			int end = edge[1] - 1;
+			int start = edge[0];
+			int end = edge[1];
 			int weight = edge[2];
-			dist[start][end] = weight;
+			graph[start].push_back({end, weight});
 		}
-		for (int k = 0; k < n; k++) {
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					if (dist[i][k] == INT_MAX || dist[k][j] == INT_MAX) {
-						continue;
-					}
-					else {
-						dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-					}
+		return graph;
+	}
+	vector<int> shortestDistance(vector<vector<pair<int, int>>>&g, int n, int src) {
+		vector<int>dist(n + 1, INT_MAX);
+		dist[src] = 0;
+		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>>q;
+		// Entry of pq will be of the form (dist,node)
+		q.push({0, src});
+		while (!q.empty()) {
+			int cur_node = q.top().second;
+			int d = q.top().first;
+			q.pop();
+			for (auto &node : g[cur_node]) {
+				int next_node = node.first;
+				int weight = node.second;
+				if (dist[cur_node] + weight < dist[next_node]) {
+					dist[next_node] = dist[cur_node] + weight;
+					q.push({dist[next_node], next_node});
 				}
 			}
 		}
