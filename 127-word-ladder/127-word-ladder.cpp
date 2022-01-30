77e78783c089045c3d8020ceb69307a622bb2360
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-01-30 23:40:12 +0530

    Time: 800 ms (27.10%), Space: 14.2 MB (57.92%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
-	void addNeighbours(string &w, unordered_set<string>&s, queue<string>&q) {
-		for (int i = 0; i < w.size(); i++) {
-			char c = w[i];
-			for (char j = 'a'; j <= 'z'; j++) {
-				if (w[i] == j) {
-					continue;
-				}
-				w[i] = j;
-				if (s.count(w)) {
-					q.push(w);
-					s.erase(w);
-				}
-			}
-			w[i] = c;
-		}
-	}
-	int ladderLength(string B, string E, vector<string>& A) {
-		unordered_set<string>s(begin(A), end(A));
-		if (s.count(E) == 0) {
-			return 0;
-		}
-		queue<string>q;
-		q.push(B);
-		int ans = 1;
-		while (q.size()) {
-			int cnt = q.size();
-			while (cnt--) {
-				auto w = q.front();
-				q.pop();
-				if (w == E) {
-					return ans;
-				}
-				addNeighbours(w, s, q);
-			}
-			ans++;
-		}
-		return 0;
-	}
+    void addWords(queue<string>&q, set<string>&s, string &w){
+        for(int i=0;i<w.size();i++){
+            char c=w[i];
+            for(char j='a';j<='z';j++){
+                w[i]=j;
+                if(w[i]!=c){
+                    if(s.count(w)){
+                        q.push(w);
+                        s.erase(w);
+                    }
+                }
+            }
+            w[i]=c;
+        }
+    }
+    int ladderLength(string B, string E, vector<string>& list) {
+        set<string>s(begin(list),end(list));
+        if(s.count(E)==0){
+            return 0;
+        }
+        queue<string>q;
+        q.push(B);
+        int ans=1;
+        while(!q.empty()){
+            int cnt=q.size();
+            while(cnt--){
+                string w=q.front();
+                q.pop();
+                if(w==E){
+                    return ans;
+                }
+                addWords(q,s,w);
+            }
+            ans++;
+        }
+        return 0;
+    }
 };
