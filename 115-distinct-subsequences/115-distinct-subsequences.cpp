72b0758af40cc8751c1f5e2cd0da30c66c1cf6bb
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-01-31 08:53:11 +0530

    Time: 98 ms (12.23%), Space: 19 MB (29.30%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int dp[1005][1005];
-    int calc(int i, int j, string &s, string &t){
-        int n=s.size(), m=t.size();
-        if(j==m){
-            return 1;
-        }
-        if(i==n){
-            return (j>=m);
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int ans=0;
-        if(s[i]==t[j]){
-            ans= calc(i+1,j,s,t)+calc(i+1,j+1,s,t);
-        }
-        else{
-            ans=calc(i+1,j,s,t);
-        }
-        return dp[i][j]=ans;
-    }
-    int numDistinct(string s, string t) {
-        memset(dp,-1,sizeof(dp));
-        return calc(0,0,s,t);
-    }
+	int numDistinct(string s, string t) {
+		int n = s.size(), m = t.size();
+		vector<vector<int>>dp(n + 5, vector<int>(m + 5, -1));
+		function<int(int, int)>calc = [&](int i, int j) {
+			if (j == m) {
+				return 1;
+			}
+			if (i == n) {
+				int v = (j >= m);
+				return v;
+			}
+			if (dp[i][j] != -1) {
+				return dp[i][j];
+			}
+			int ans = 0;
+			if (s[i] == t[j]) {
+				ans = calc(i + 1, j) + calc(i + 1, j + 1);
+			}
+			else {
+				ans = calc(i + 1, j);
+			}
+			return dp[i][j] = ans;
+		};
+		return calc(0, 0);
+	}
 };
