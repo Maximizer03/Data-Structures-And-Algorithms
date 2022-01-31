b933bf9ef86eae360be7b8d75b418551709128f3
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-01-31 09:09:10 +0530

    Time: 97 ms (12.40%), Space: 30 MB (21.22%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -2,26 +2,23 @@ class Solution {
 public:
 	int numDistinct(string s, string t) {
 		int n = s.size(), m = t.size();
-		vector<vector<int>>dp(n + 5, vector<int>(m + 5, -1));
-		function<int(int, int)>calc = [&](int i, int j) {
-			if (j == m) {
-				return 1;
-			}
-			if (i == n) {
-				return int(j>=m);
-			}
-			if (dp[i][j] != -1) {
-				return dp[i][j];
-			}
-			int ans = 0;
-			if (s[i] == t[j]) {
-				ans = calc(i + 1, j) + calc(i + 1, j + 1);
-			}
-			else {
-				ans = calc(i + 1, j);
-			}
-			return dp[i][j] = ans;
-		};
-		return calc(0, 0);
+		vector<vector<long long>>dp(n+1,vector<long long>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+                if(dp[i][j]>INT_MAX){
+                    dp[i][j]=0;
+                }
+            }
+        }
+        return dp[n][m];
 	}
 };
