152737629f2f7d4791622058cb3aad5d39a8dede
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-02-01 09:56:30 +0530

    Time: 768 ms (12.27%), Space: 387.3 MB (5.33%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,7 +1,9 @@
 class Solution {
+private:
+    vector<vector<int>>dp;
+    string s;
 public:
-    int dp[1005][1005];
-    int calc (int i, int j,string &s) {
+    int calc (int i, int j) {
 			if (i == j) {
 				return 1;
 			}
@@ -11,18 +13,18 @@ public:
 			if (dp[i][j] != -1) {
 				return dp[i][j];
 			}
-			int ans = ((s[i] == s[j]) && calc(i + 1, j - 1, s));
+			int ans = ((s[i] == s[j]) && calc(i + 1, j - 1));
 			return dp[i][j] = ans;
 		}
-	string longestPalindrome(string s) {
-		int n = s.size();
-        memset(dp,-1,sizeof(dp));
-		vector<vector<int>>dp(n, vector<int>(n, -1));
+	string longestPalindrome(string k) {
+        s=k;
+        int n = s.size();
+        dp=vector<vector<int>>(n,vector<int>(n,-1));
 		// dp[i][j]=> if the substring s[i,j] is a palindrome
 		int mx = 1, st = 0, en = 0;
 		for (int i = 0; i < n; i++) {
 			for (int j = i; j < n; j++) {
-				if (calc(i, j, s)) {
+				if (calc(i, j)) {
 					int len = j - i + 1;
 					if (len > mx) {
 						mx = len;
