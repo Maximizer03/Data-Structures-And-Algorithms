ee0fd52e9454f5c32a75779703ef83cc8fa7143a
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-02-01 13:14:21 +0530

    Time: 483 ms (19.20%), Space: 50.4 MB (38.33%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
-    bool canCross(vector<int>& a) {
-        int n=a.size();
-        map<int,set<int>>m;
-        m[0].insert(1);
-        for(int i=0;i<n;i++){
-            for(auto &x:m[a[i]]){
-                // cout<<x<<" "<<a[i]<<endl;
-                int next= x+a[i];
-                if(binary_search(a.begin(),a.end(),next)){
-                    for(int j=x-1;j<=x+1;j++){
-                        if(j>0){
-                            m[next].insert(j);
-                        }
-                    }
-                }
-            }
-        }
-        int lst= a[n-1];
-        if(m[lst].size()){
-            return 1;
-        }
-        else{
-            return 0;
-        }
-    }
+	bool canCross(vector<int>& a) {
+		int n = a.size();
+		map<int, set<int>>m;
+		m[0].insert(1);
+		for (int i = 0; i < n; i++) {
+			for (auto &x : m[a[i]]) {
+				int next = x + a[i];
+				if (binary_search(a.begin(), a.end(), next)) {
+					for (int j = x - 1; j <= x + 1; j++) {
+						if (j > 0) {
+							m[next].insert(j);
+						}
+					}
+				}
+			}
+		}
+		return (m[a[n-1]].size());
+	}
 };
