2c7d8f7bc51cd1edee70999a63db4dbabdedb67b
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-02-02 20:47:46 +0530

    Time: 4 ms (75.44%), Space: 7.4 MB (35.81%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,55 +1,55 @@
 class Solution {
 public:
-    int calc(string s, int k){
-        int n=s.size();
-        if(k>n){
-            return 0;
-        }
-        vector<int>cnt(26,0);
-        for(int i=0;i<n;i++){
-            cnt[s[i]-'a']++;
-        }
-        int flg=1;
-        for(int i=0;i<26;i++){
-            if(cnt[i]>0){
-                flg = (flg & (cnt[i]>=k) );
-            }
-        }
-        return (flg==1)?s.size():0;
-    }
-    int longestSubstring(string s, int k) {
-        int n=s.size();
-        if(k>n){
-            return 0;
-        }
-        vector<int>cnt(26,0);
-        for(int i=0;i<n;i++){
-            cnt[s[i]-'a']++;
-        }
-        int i=0;
-        vector<string>a;
-        string t="";
-        while(i<n){
-            if(cnt[s[i]-'a']>=k){
-                t+=s[i];
-                i++;
-            }
-            else{
-                a.push_back(t);
-                t="";
-                i++;
-            }
-        }
-        if(t.size()){
-            a.push_back(t);
-        }
-        int mx=0;
-        for(auto &x:a){
-            mx=max(mx,calc(x,k));
-            if(calc(x,k)==0){
-                mx=max(mx,longestSubstring(x,k));
-            }
-        }
-        return mx;
-    }
+	int calc(string s, int k) {
+		int n = s.size();
+		if (k > n) {
+			return 0;
+		}
+		vector<int>cnt(26, 0);
+		for (int i = 0; i < n; i++) {
+			cnt[s[i] - 'a']++;
+		}
+		int flg = 1;
+		for (int i = 0; i < 26; i++) {
+			if (cnt[i] > 0) {
+				flg = (flg & (cnt[i] >= k) );
+			}
+		}
+		return (flg == 1) ? s.size() : 0;
+	}
+	int longestSubstring(string s, int k) {
+		int n = s.size();
+		if (k > n) {
+			return 0;
+		}
+		vector<int>cnt(26, 0);
+		for (int i = 0; i < n; i++) {
+			cnt[s[i] - 'a']++;
+		}
+		int i = 0;
+		vector<string>a;
+		string t = "";
+		while (i < n) {
+			if (cnt[s[i] - 'a'] >= k) {
+				t += s[i];
+				i++;
+			}
+			else {
+				a.push_back(t);
+				t = "";
+				i++;
+			}
+		}
+		if (t.size()) {
+			a.push_back(t);
+		}
+		int mx = 0;
+		for (auto &x : a) {
+			mx = max(mx, calc(x, k));
+			if (calc(x, k) == 0) {
+				mx = max(mx, longestSubstring(x, k));
+			}
+		}
+		return mx;
+	}
 };
