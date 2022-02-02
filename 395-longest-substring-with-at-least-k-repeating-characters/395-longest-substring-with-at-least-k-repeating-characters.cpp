e6701fafa3da237b1600ce9575cc84ade40e34f7
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-02-02 20:27:07 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (52.19%) - LeetHub

---
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int longestSubstring(string s, int k) {
+        int n=s.size();
+        if(k>n){
+            return 0;
+        }
+        vector<int>cnt(26,0);
+        for(int i=0;i<n;i++){
+            cnt[s[i]-'a']++;
+        }
+        int l=0;
+        while(l<n){
+            if(cnt[s[l]-'a']>=k){
+                l++;
+            }
+            else{
+                break;
+            }
+        }
+        if(l>=n-1){
+           return l;
+        }
+        string t="";
+        for(int i=0;i<l;i++){
+            t+=s[i];
+        }
+        int a= longestSubstring(t,k);
+        int b=0;
+        while(l<n && cnt[s[l]-'a']<k){
+            l++;
+        }
+        if(l==n){
+            b=0;
+        }
+        else{
+            string r="";
+            for(int i=l;i<n;i++){
+                r+=s[i];
+            }
+            b=longestSubstring(r,k);
+        }
+        return max(a,b);
+    }
+};
