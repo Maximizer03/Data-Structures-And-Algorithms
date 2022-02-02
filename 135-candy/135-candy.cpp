1dfc1dc2908073dc6244aabf30aa63f320deadf0
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-02-02 16:05:45 +0530

    Time: 47 ms (15.98%), Space: 18.3 MB (13.09%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,62 +1,22 @@
 class Solution {
 public:
-    void dfs(int v, int p, vector<vector<int>>&g, int st, vector<int>&dp,vector<int>&vis){
-        dp[v]=max(dp[v],st);
-        vis[v]=1;
-        for(auto u:g[v]){
-            if(u!=p){
-                dfs(u,v,g,st+1,dp,vis);
-            }
-        }
-    }
     int candy(vector<int>& r) {
         int n=r.size();
-        vector<int>dp(n,0);
-        vector<vector<int>>g(n);
-        vector<int>in(n,0);
+        vector<int>left(n,1),right(n,1);
+        for(int i=1;i<n;i++){
+            if(r[i]>r[i-1]){
+                left[i]=left[i-1]+1;
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(r[i]>r[i+1]){
+                right[i]=right[i+1]+1;
+            }
+        }
+        int ans=0;
         for(int i=0;i<n;i++){
-            if(i<n-1){
-                if(r[i]>r[i+1]){
-                    g[i+1].push_back(i);
-                    in[i]++;
-                }
-            }
-            if(i-1>=0){
-                if(r[i]>r[i-1]){
-                    g[i-1].push_back(i);
-                    in[i]++;
-                }
-            }
+            ans+=max(left[i],right[i]);
         }
-        vector<int>o;
-        queue<int>q;
-        for(int i=0;i<n;i++){
-            if(in[i]==0){
-                q.push(i);
-            }
-        }
-        while(!q.empty()){
-            int v=q.front();
-            q.pop();
-            o.push_back(v);
-            for(auto &u:g[v]){
-                in[u]--;
-                if(in[u]==0){
-                    q.push(u);
-                }
-            }
-        }
-        vector<int>vis(n,0);
-        for(int i=0;i<n;i++){
-            int v= o[i];
-            if(!vis[v]){
-                dfs(v,-1,g,1,dp,vis);
-            }
-        }
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=dp[i];
-        }
-        return sum;
+        return ans;
     }
 };
