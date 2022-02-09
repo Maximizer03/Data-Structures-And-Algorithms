d9646c95417b448b1cf201c4d6a76fec06953f59
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-02-09 19:53:41 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -5,31 +5,24 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
   public:
-    long long calc(int i, int sum, int n, int m,vector<int>&c,vector<vector<long long>>&dp){
-        if(i==m){
-            return (sum==n);
-        }
-        if(sum>n){
-            return 0;
-        }
-        if(sum==n){
-            return 1;
-        }
-        if(dp[i][sum]!=-1){
-            return dp[i][sum];
-        }
-        long long ans=0;
-        ans+=calc(i,sum+c[i],n,m,c,dp);
-        ans+=calc(i+1,sum,n,m,c,dp);
-        return dp[i][sum]=ans;
-    }
     long long int count(int S[], int m, int n) {
         vector<int>c;
         for(int i=0;i<m;i++){
             c.push_back(S[i]);
         }
-        vector<vector<long long>>dp(m+1,vector<long long>(n+1,-1));
-        return calc(0,0,n,m,c,dp);
+        vector<vector<long long>>dp(m+1,vector<long long>(n+1,0));
+        for(int i=0;i<=m;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                dp[i][j]=dp[i-1][j];
+                if(j-c[i-1]>=0){
+                    dp[i][j]+=dp[i][j-c[i-1]];
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
 
