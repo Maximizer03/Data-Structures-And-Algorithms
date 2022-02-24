352fcfc9bd8d7c5216b71cb807e4bc30459309b4
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-02-24 16:23:36 +0530

    Time: 4 ms (75.44%), Space: 11.7 MB (26.95%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    int dp[5005][2][2];
-    int calc(int i, int own, int cool, vector<int>&prices){
-        if(i==prices.size()){
-            return 0;
-        }
-        if(dp[i][own][cool]!=-1){
-            return dp[i][own][cool];
-        }
-        if(own){
-            int op1=calc(i+1,0,1,prices)+prices[i];
-            int op2= calc(i+1,1,0,prices);
-            return dp[i][own][cool]=max(op1,op2);
-        }
-        else{
-            int op1=0;
-            if(!cool){
-                op1=-prices[i]+calc(i+1,1,0,prices);
-            }
-            int op2= calc(i+1,0,0,prices);
-            return dp[i][own][cool]=max(op1,op2);
-        }
-    }
-    int maxProfit(vector<int>& prices) {
-        memset(dp,-1,sizeof(dp));
-        return calc(0,0,0,prices);
-    }
+	int dp[5005][2][2];
+	int calc(int i, int own, int cool, vector<int>&prices) {
+		if (i == prices.size()) {
+			return 0;
+		}
+		if (dp[i][own][cool] != -1) {
+			return dp[i][own][cool];
+		}
+		if (own) {
+			int op1 = calc(i + 1, 0, 1, prices) + prices[i];
+			int op2 = calc(i + 1, 1, 0, prices);
+			return dp[i][own][cool] = max(op1, op2);
+		}
+		else {
+			int op1 = 0;
+			if (!cool) {
+				op1 = -prices[i] + calc(i + 1, 1, 0, prices);
+			}
+			int op2 = calc(i + 1, 0, 0, prices);
+			return dp[i][own][cool] = max(op1, op2);
+		}
+	}
+	int maxProfit(vector<int>& prices) {
+		memset(dp, -1, sizeof(dp));
+		return calc(0, 0, 0, prices);
+	}
 };
