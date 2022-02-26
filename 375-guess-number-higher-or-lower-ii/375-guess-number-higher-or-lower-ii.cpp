aab5a7eabbe2ba27463fb0e704e5e07b7e96bbab
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-02-26 12:34:20 +0530

    Time: 200 ms (45.39%), Space: 8.3 MB (36.97%) - LeetHub

--- 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
+    int calc(int i, int j, int n,vector<vector<int>>&dp){
+        if(i>=j){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int ans=1e9;
+        for(int k=i;k<=j;k++){
+            ans=min(ans,k+   max(  calc(i,k-1,n,dp),calc(k+1,j,n,dp) )  );
+        }
+        return dp[i][j]=ans;
+    }
     int getMoneyAmount(int n) {
-        // dp[i][j]=> min cost to guarantee win for range [i,j]
-        // dp[i][j]= min(max(dp[i][pivot-1], dp[pivot+1][j])+pivot);
-        vector<vector<int>>dp(n+5,vector<int>(n+5,0));
-        for(int i=1;i<=n;i++){
-            dp[i][i]=0;
-        }
-        for(int l=2;l<=n;l++){
-            for(int s=1;s<=n-l+1;s++){
-                int e=s+l-1;
-                int ans=INT_MAX;
-                for(int p=s;p<=e;p++){
-                    ans=min(ans,max(dp[s][p-1],dp[p+1][e])+p);
-                }
-                dp[s][e]=ans;
-            }
-        }
-        return dp[1][n];
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return calc(1,n,n,dp);
     }
 };
