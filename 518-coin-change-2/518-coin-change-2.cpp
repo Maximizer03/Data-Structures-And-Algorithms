60aa0050202a6104e3356bde690eca9e961325f7
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-03-02 16:03:59 +0530

    Time: 41 ms (39.25%), Space: 18.3 MB (36.85%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int change(int sum, vector<int>& coins) {
-        int n=coins.size();
-        vector<int>prev(sum+1,0);
-        prev[0]=1;
-        for(int i=1;i<=n;i++){
-            vector<int>cur(sum+1,0);
-            cur[0]=1;
-            for(int j=1;j<=sum;j++){
-                cur[j]=prev[j];
-                if(j-coins[i-1]>=0){
-                    cur[j]+=cur[j-coins[i-1]];
-                }
-            }
-            prev=cur;
-        }
-        return prev.back();
-    }
+	int change(int sum, vector<int>& coins) {
+		int n = coins.size();
+		vector<int>prev(sum + 1, 0);
+		prev[0] = 1;
+		for (int i = 1; i <= n; i++) {
+			vector<int>cur(sum + 1, 0);
+			cur[0] = 1;
+			for (int j = 1; j <= sum; j++) {
+				cur[j] = prev[j];
+				if (j - coins[i - 1] >= 0) {
+					cur[j] += cur[j - coins[i - 1]];
+				}
+			}
+			prev = cur;
+		}
+		return prev.back();
+	}
 };
