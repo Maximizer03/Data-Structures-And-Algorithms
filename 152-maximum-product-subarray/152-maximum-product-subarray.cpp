f1bb2898cace17e3c0a0db7ee51253174e36c032
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-03-02 15:23:51 +0530

    Time: 8 ms (71.73%), Space: 13.9 MB (10.66%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp1(n,-1e9);
-        vector<int>dp2(n,1e9);
-        // dp1[i]=> max product subarray ending at i
-        // dp2[i]=> min prduct subarray ending at i
-        dp1[0]=dp2[0]=nums[0];
+        int pmaxi=nums[0];
+        int pmini=nums[0];
         int mx=nums[0];
         for(int i=1;i<n;i++){
-            dp1[i]=max({nums[i],nums[i]*dp1[i-1],nums[i]*dp2[i-1]});
-            dp2[i]=min({nums[i],nums[i]*dp2[i-1],nums[i]*dp1[i-1]});
-            mx=max({mx,dp1[i],dp2[i]});
+            int cmaxi=max({nums[i],nums[i]*pmaxi,nums[i]*pmini});
+            int cmini=min({nums[i],nums[i]*pmaxi,nums[i]*pmini});
+            mx=max({mx,cmaxi,cmini});
+            pmaxi=cmaxi;
+            pmini=cmini;
         }
         return mx;
     }
