2058d4a8e8f65d1d24dcf4228f677e8437c237de
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-03-03 11:55:30 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (77.43%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    int calc(int i, int cnt, vector<vector<int>>&costs,vector<vector<int>>&dp){
-        int n=costs.size();
-        if(cnt>n/2){
-            return 1e5;
-        }
-        if(i==n){
-            if(cnt==n/2){
-                return 0;
-            }
-            else{
-                return 1e5;
-            }
-        }
-        if(dp[i][cnt]!=-1){
-            return dp[i][cnt];
-        }
-        int ans=min(calc(i+1,cnt+1,costs,dp)+costs[i][0],calc(i+1,cnt,costs,dp)+costs[i][1]);
-        return dp[i][cnt]=ans;
-    }
+    struct cmp{
+      bool operator()(const pair<int,int>&a, const pair<int,int>&b){
+          return (a.first-a.second > b.first-b.second);
+      }
+    };
     int twoCitySchedCost(vector<vector<int>>& costs) {
         int n=costs.size();
-        vector<vector<int>>dp(n+1,vector<int>(n/2+1,-1));
-        return calc(0,0,costs,dp);
+        using pii= pair<int,int>;
+        priority_queue<pii,vector<pii>,cmp>q;
+      
+        for(int i=0;i<n;i++){
+            int a= costs[i][0];
+            int b= costs[i][1];
+            q.push({a,b});
+        }
+        int cnt=0,ans=0;
+        while(!q.empty()){
+            if(cnt<n/2){
+                ans+=q.top().first;
+            }
+            else{
+                ans+=q.top().second;
+            }
+            cnt++;
+            q.pop();
+        }
+        return ans;
     }
 };
