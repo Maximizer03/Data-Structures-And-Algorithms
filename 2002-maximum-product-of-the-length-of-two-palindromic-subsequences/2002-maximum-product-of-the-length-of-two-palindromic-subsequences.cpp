6a0573e9cd0326c6f748ab6965cc92eb8735e4cf
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-03-03 14:33:30 +0530

    Time: 430 ms (53.50%), Space: 6.1 MB (49.11%) - LeetHub

--- 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
@@ -1,55 +1,42 @@
 class Solution {
 public:
-	int longestPalindromeSubseq(string s) {
-        if(s.size()==0){
-            return 0;
+    int ans;
+    bool isPalindrome(string &s){
+        bool ans=true;
+        int l=0,r=s.size()-1;
+        while(l<r){
+            if(s[l]==s[r]){
+                l++;r--;
+            }
+            else{
+                return false;
+            }
         }
-		int n = s.size();
-		vector<int>cur(n, 0), prev(n, 0);
-		prev[n - 1] = 1;
-		for (int i = n - 2; i >= 0; i--) {
-			for (int j = i; j < n; j++) {
-				if (i == j) {
-					cur[j] = 1;
-					continue;
-				}
-				if (s[i] == s[j]) {
-					cur[j] = prev[j - 1] + 2;
-				}
-				else {
-					cur[j] = max(cur[j - 1], prev[j]);
-				}
-			}
-			swap(cur, prev);
-		}
-		return prev[n - 1];
-	}
-	int ans;
-	void calc(int i, string s, int mask, int n) {
-		if (i == n) {
-			string s1 = "";
-			string s2 = "";
-			for (int j = 0; j < n; j++) {
-				int bit = (1 << j)&mask;
-				if (bit > 0) {
-					s1 += s[j];
-				}
-				else {
-					s2 += s[j];
-				}
-			}
-			ans = max(ans, longestPalindromeSubseq(s1) * longestPalindromeSubseq(s2));
-			return;
-		}
-		int newMask = (mask | (1 << i));
-		calc(i + 1, s, newMask,n);
-		calc(i + 1, s, mask,n);
-	}
-	int maxProduct(string s) {
-		int mask = 0;
-		ans = 0;
-        int n=s.size();
-		calc(0, s, mask,n);
-		return ans;
-	}
+        return ans;
+    }
+    void calc(int i, string &s, string &s1, string &s2){
+        if(i==s.size()){
+            if(isPalindrome(s1) && isPalindrome(s2)){
+                int t1=s1.size();
+                int t2= s2.size();
+                ans=max(ans,t1*t2);
+            }
+            return;
+        }
+        s1.push_back(s[i]);
+        calc(i+1,s,s1,s2);
+        s1.pop_back();
+        
+        s2.push_back(s[i]);
+        calc(i+1,s,s1,s2);
+        s2.pop_back();
+        
+        calc(i+1,s,s1,s2);
+    }
+    int maxProduct(string s) {
+        string s1="",s2="";
+        ans=0;
+        calc(0,s,s1,s2);
+        return ans;
+    }
 };
