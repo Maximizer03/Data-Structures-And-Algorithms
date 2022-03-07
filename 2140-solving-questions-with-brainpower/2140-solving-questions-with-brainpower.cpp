0165803abeaf782000ceb7ca024a86ea187785e6
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-03-07 17:55:16 +0530

    Time: 422 ms (72.75%), Space: 115.3 MB (56.98%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    long long mostPoints(vector<vector<int>>& questions) {
-        int n=questions.size();
-        vector<int>p(n),sk(n);
-        for(int i=0;i<n;i++){
-            p[i]=questions[i][0];
-            sk[i]=questions[i][1];
-        }
-        vector<long long>dp(n,0);
-        dp[n-1]=p[n-1];
-        for(int i=n-2;i>=0;i--){
-            dp[i]=dp[i+1];
-            long long prev=0;
-            if(i+sk[i]+1<n){
-                prev=dp[i+sk[i]+1];
-            }
-            
-            dp[i]=max(dp[i],prev+p[i]);
-            
-        }
-        return dp[0];
-    }
+	long long mostPoints(vector<vector<int>>& questions) {
+		int n = questions.size();
+		vector<long long>dp(n, 0);
+		dp[n - 1] = questions[n - 1][0];
+		for (int i = n - 2; i >= 0; i--) {
+			dp[i] = dp[i + 1];
+			long long prev = 0;
+			if (i + questions[i][1] + 1 < n) {
+				prev = dp[i + questions[i][1] + 1];
+			}
+			dp[i] = max(dp[i], prev + questions[i][0]);
+		}
+		return dp[0];
+	}
 };
