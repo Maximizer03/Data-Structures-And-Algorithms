a3825e2d087e5fe940f6a71af49e5b434a7ce4a5
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-03-15 15:27:20 +0530

    Time: 854 ms (68.13%), Space: 19.4 MB (27.46%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    int minimizeTheDifference(vector<vector<int>>& mat, int target) {
-        int n=mat.size(),m=mat[0].size();
-        bitset<70*70+1>bit;
-        int mx=0;
-        for(int i=0;i<n;i++){
-            bitset<70*70+1>cur_bit;
-            int cur_mx=0;
-            for(auto &x: unordered_set(begin(mat[i]),end(mat[i]))){
-                for(int j=0;j<=mx;j++){
-                    if(bit[j]==true || j==mx){
-                        cur_bit[x+j]=true;
-                        cur_mx=max(cur_mx,x+j);
-                    }
-                }
-            }
-            bit=cur_bit;
-            mx=cur_mx;
-        }
-        int ans=1e9;
-        for(int i=0;i<= mx;i++){
-            if(bit[i]==true){
-                ans=min(ans,abs(target-i));
-            }
-        }
-        return ans;
-    }
+	int minimizeTheDifference(vector<vector<int>>& mat, int target) {
+		int n = mat.size(), m = mat[0].size();
+		vector<bool>bit(4901);
+		int mx = 0;
+		for (int i = 0; i < n; i++) {
+			vector<bool>cur_bit(4901);
+			int cur_mx = 0;
+			for (auto &x : unordered_set(begin(mat[i]), end(mat[i]))) {
+				for (int j = 0; j <= mx; j++) {
+					if (bit[j] == true || j == mx) {
+						cur_bit[x + j] = true;
+						cur_mx = max(cur_mx, x + j);
+					}
+				}
+			}
+			bit = cur_bit;
+			mx = cur_mx;
+		}
+		int ans = 1e9;
+		for (int i = 0; i < 4901; i++) {
+			if (bit[i] == true) {
+				ans = min(ans, abs(target - i));
+			}
+		}
+		return ans;
+	}
 };
