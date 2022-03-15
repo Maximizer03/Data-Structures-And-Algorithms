f3810af4740af8af5dfdac2004907f934a6dfb88
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-03-15 17:40:27 +0530

    Time: 150 ms (94.72%), Space: 16.9 MB (36.27%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
 	int minimizeTheDifference(vector<vector<int>>& mat, int target) {
 		int n = mat.size(), m = mat[0].size();
-		vector<bool>bit(4901);
-		int mx = 0;
-		for (int i = 0; i < n; i++) {
-			vector<bool>cur_bit(4901);
-			int cur_mx = 0;
+		bitset < 70 * 70 + 1 > b;
+		for (int i = 0; i < m; i++) {
+			b[mat[0][i]] = true;
+		}
+		for (int i = 1; i < n; i++) {
+			bitset<4901>c;
 			for (auto &x : unordered_set(begin(mat[i]), end(mat[i]))) {
-				for (int j = 0; j <= mx; j++) {
-					if (bit[j] == true || j == mx) {
-						cur_bit[x + j] = true;
-						cur_mx = max(cur_mx, x + j);
-					}
-				}
+				c = c | (b << x);
 			}
-			bit = cur_bit;
-			mx = cur_mx;
+			b = c;
 		}
 		int ans = 1e9;
 		for (int i = 0; i < 4901; i++) {
-			if (bit[i] == true) {
+			if (b[i] == true) {
 				ans = min(ans, abs(target - i));
 			}
 		}
