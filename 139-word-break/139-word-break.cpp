3b106daa919ecf4ccffc656b5cf61cb13e63c13a
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-03-17 08:50:56 +0530

    Time: 11 ms (63.94%), Space: 9.5 MB (66.08%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    int calc(int i, string s, unordered_map<string,int>&m,vector<int>&dp){
-        int n=s.size();
-        if(i==n){
-            return 1;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int ans=0;
-        string t="";
-        for(int j=i;j<n;j++){
-            t+=s[j];
-            if(m.find(t)!=m.end()){
-                ans= ans | calc(j+1,s,m,dp);
-            }
-        }
-        return dp[i]=ans;
-    }
-    bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_map<string,int>m;
-        for(auto &word:wordDict){
-            m[word]++;
-        }
-        vector<int>dp(s.size(),-1);
-        return calc(0,s,m,dp);
-    }
+	int calc(int i, string s, unordered_map<string, int>&m, vector<int>&dp) {
+		int n = s.size();
+		if (i == n) {
+			return 1;
+		}
+		if (dp[i] != -1) {
+			return dp[i];
+		}
+		int ans = 0;
+		string t = "";
+		for (int j = i; j < n; j++) {
+			t += s[j];
+			if (m.find(t) != m.end()) {
+				ans = ans | calc(j + 1, s, m, dp);
+			}
+		}
+		return dp[i] = ans;
+	}
+	bool wordBreak(string s, vector<string>& wordDict) {
+		unordered_map<string, int>m;
+		for (auto &word : wordDict) {
+			m[word]++;
+		}
+		vector<int>dp(s.size(), -1);
+		return calc(0, s, m, dp);
+	}
 };
