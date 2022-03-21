77852e6d70677664f5bb28ca0350a9313f6e7f1f
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-03-21 13:49:53 +0530

    Time: 4 ms (23.56%), Space: 5.9 MB (72.58%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
-     int uniquePaths(int n, int m) {
-          long long ans = 1;
-          for (int i = m + n - 2, j = 1; i >= max(n,m); i--, j++) {
-               ans = (ans * i) / j;
-          }
-          return ans;
-     }
+    int uniquePaths(int n, int m) {
+        if(n==1 || m==1){
+            return 1;
+        }
+        if(n<m){
+            swap(n,m);
+        }
+        n--;
+        m--;
+        long long ans=1;
+        int j=1;
+        for(int i=n+1;i<=n+m;i++){
+            ans*=i;
+            ans/=j;
+            j++;
+        }
+        return (int)ans;
+    }
 };
