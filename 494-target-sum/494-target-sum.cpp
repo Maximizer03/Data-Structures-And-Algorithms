a49761ae6e0aacf8901f37f62ce3a6e88202e445
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-03-21 14:25:24 +0530

    Time: 315 ms (32.68%), Space: 61.1 MB (13.82%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int calc(int i, vector<int>&nums,int sum,vector<unordered_map<int,int>>&dp){
-        if(i==nums.size()){
-            return sum==0;
-        }
-        if(dp[i].find(sum)!=dp[i].end()){
-            return dp[i][sum];
-        }
-        int ans= calc(i+1,nums,sum-nums[i],dp)+calc(i+1,nums,sum+nums[i],dp);
-        return dp[i][sum]=ans;
-    }
-    int findTargetSumWays(vector<int>& nums, int sum) {
-        vector<unordered_map<int,int>>dp(nums.size());
-        return calc(0,nums,sum,dp);
-    }
+	int calc(int i, vector<int>&nums, int sum, vector<unordered_map<int, int>>&dp) {
+		if (i == nums.size()) {
+			return sum == 0;
+		}
+		if (dp[i].find(sum) != dp[i].end()) {
+			return dp[i][sum];
+		}
+		int ans = calc(i + 1, nums, sum - nums[i], dp) + calc(i + 1, nums, sum + nums[i], dp);
+		return dp[i][sum] = ans;
+	}
+	int findTargetSumWays(vector<int>& nums, int sum) {
+		vector<unordered_map<int, int>>dp(nums.size());
+		return calc(0, nums, sum, dp);
+	}
 };
