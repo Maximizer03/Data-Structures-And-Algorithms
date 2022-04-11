3fd613d62c6aeea25f3bb085efb29c14c6863ea7
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-04-11 18:54:02 +0530

    Time: 127 ms (29.14%), Space: 42 MB (9.41%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+	int numRescueBoats(vector<int>& P, int L) {
+		int n = P.size();
+		sort(begin(P), end(P));
+		int ans = 0;
+		while (P.back() == L) {
+			ans++;
+			P.pop_back();
+		}
+		n = P.size();
+		int l = 0, r = n - 1;
+		while (l < r) {
+			int sum = P[l] + P[r];
+			if (sum > L) {
+				ans++;
+				r--;
+			}
+			else {
+				l++;
+				r--;
+				ans++;
+			}
+		}
+		if (l == r) {
+			ans++;
+		}
+		return ans;
+	}
+};
