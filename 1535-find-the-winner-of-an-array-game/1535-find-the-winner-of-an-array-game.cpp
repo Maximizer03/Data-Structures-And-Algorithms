f355d4a3db5a9be48613e948281b106a77f3be32
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-04-11 13:22:35 +0530

    Time: 128 ms (71.28%), Space: 70 MB (10.04%) - LeetHub

---
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int getWinner(vector<int>& arr, int k) {
+        int mx=0,n=arr.size();
+        for(int i=0;i<n;i++){
+            mx=max(mx,arr[i]);
+        }
+        deque<int>d;
+        for(int i=0;i<n;i++){
+            d.push_back(arr[i]);
+        }
+        map<int,int>m;
+        while(d.front()!=mx){
+            int x1= d.front();
+            d.pop_front();
+            int x2=d.front();
+            d.pop_front();
+            if(x1>x2){
+                d.push_back(x2);
+                d.push_front(x1);
+                m[x1]++;
+                if(m[x1]==k){
+                    return x1;
+                }
+            }
+            else{
+                d.push_back(x1);
+                d.push_front(x2);
+                m[x2]++;
+                if(m[x2]==k){
+                    return x2;
+                }
+            }
+        }
+        return d.front();
+    }
+};
