0f2b53c977a4d047ccb2ac87fa51158610421b3c
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-05-03 11:19:36 +0530

    Time: 1727 ms (21.05%), Space: 319.1 MB (14.74%) - LeetHub

--- 898-bitwise-ors-of-subarrays/898-bitwise-ors-of-subarrays.cpp
+++ 898-bitwise-ors-of-subarrays/898-bitwise-ors-of-subarrays.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int subarrayBitwiseORs(vector<int>& arr) {
-        unordered_set<int>s;
-        set<int>cur;
-        for(auto x:arr){
-            set<int>t;
-            t.insert(x);
-            for(auto y:cur){
-                t.insert((y|x));
-            }
-            cur=t;
-            for(auto x:cur){
-                s.insert(x);
-            }
-        }
-        return s.size();
-    }
+	int subarrayBitwiseORs(vector<int>& arr) {
+		unordered_set<int>s;
+		unordered_set<int>cur;
+		for (auto x : arr) {
+			unordered_set<int>t;
+			t.insert(x);
+			for (auto y : cur) {
+				t.insert((y | x));
+			}
+			cur = t;
+			for (auto x : cur) {
+				s.insert(x);
+			}
+		}
+		return s.size();
+	}
 };
