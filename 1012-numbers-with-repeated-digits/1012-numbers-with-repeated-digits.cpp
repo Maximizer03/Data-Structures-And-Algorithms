8cf9b976992f00b90970a185b64fbb1948818ed1
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-05-04 14:58:14 +0530

    Time: 16 ms (51.79%), Space: 6.1 MB (77.68%) - LeetHub

--- 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.cpp
+++ 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
-	int vis[11];
-	int dp[11][2][2];
-	int calc(int i, int n, int tight, int lead, string &num) {
+	int dp[11][2][2][1024];
+	int calc(int i, int n, int tight, int lead, int mask, string &num) {
 		if (i == n) {
 			return 1;
 		}
-		if (dp[i][tight][lead] != -1) {
-			return dp[i][tight][lead];
+		if (dp[i][tight][lead][mask] != -1) {
+			return dp[i][tight][lead][mask];
 		}
 		int ub = tight ? num[i] - '0' : 9;
 		int ans = 0;
 		if (!lead) {
-			if (!vis[0]) {
-				vis[0] = 1;
-
+			int bit = (1 << 0)&mask;
+			if (bit == 0) {
 				int no = num[i] - '0';
 				int z = 0;
-				ans += calc(i + 1, n, (tight & (no == z)), 0, num);
-
-				vis[0] = 0;
+				ans += calc(i + 1, n, (tight & (no == z)), 0, (mask | (1 << 0)), num);
 			}
 		}
 		else {
-			ans += calc(i + 1, n, 0, 1, num);
+			ans += calc(i + 1, n, 0, 1, mask, num);
 		}
 		for (int dig = 1; dig <= ub; dig++) {
-			if (vis[dig] == 0) {
-				vis[dig] = 1;
+			int bit = (1 << dig)&mask;
+			if (bit == 0) {
 				int no = num[i] - '0';
-				ans += calc(i + 1, n, ((tight) & (no == dig)), 0, num);
-				vis[dig] = 0;
+				ans += calc(i + 1, n, ((tight) & (no == dig)), 0, (mask | (1 << dig)), num);
 			}
 		}
-		return ans;
+		return dp[i][tight][lead][mask] = ans;
 
 	}
 	int numDupDigitsAtMostN(int R) {
 		string num = to_string(R);
-		memset(vis, 0, sizeof(vis));
 		memset(dp, -1, sizeof(dp));
-		return R - calc(0, num.size(), 1, 1, num) + 1;
+		return R - calc(0, num.size(), 1, 1, 0, num) + 1;
 	}
 };
