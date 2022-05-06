e8c08d5dc1f904242e3b3f82495fe3286321d2fa
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-05-07 00:16:24 +0530

    Time: 1732 ms (5.02%), Space: 311.3 MB (5.07%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+	vector<vector<int>> combine(int n, int k) {
+		vector<vector<int>>ans;
+		unordered_map<int, int>m;
+		for (int i = 0; i < (1 << n); i++) {
+			int cnt = 0;
+			vector<int>v;
+			for (int j = 0; j < n; j++) {
+				int bit = (1 << j)&i;
+				if (bit) {
+					cnt++;
+					v.push_back(j + 1);
+				}
+			}
+			if (cnt == k && m.find(i) == m.end()) {
+				ans.push_back(v);
+				m[i]++;
+			}
+		}
+		return ans;
+	}
+};
