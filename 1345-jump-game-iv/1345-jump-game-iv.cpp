effc99fd56b95a4d70b1a565556961d3f8cf1257
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-05-08 00:24:24 +0530

    Time: 550 ms (17.62%), Space: 140.1 MB (6.20%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
-    int minJumps(vector<int>& arr) {
-        int n=arr.size();
-        unordered_map<int,vector<int>>m;
-        for(int i=0;i<n;i++){
-            m[arr[i]].push_back(i);
-        }
-        queue<int>q;
-        q.push(0);
-        int steps=0;
-        set<int>seen;
-        seen.insert(0);
-        while(q.size()>0){
-            int s=q.size();
-            for(int i=0;i<s;i++){
-                int v=q.front();
-                q.pop();
-                if(v==n-1){
-                    return steps;
-                }
-                vector<int>pos;
-                if(v){
-                    pos.push_back(v-1);
-                }
-                pos.push_back(v+1);
-                int e= arr[v];
-                if(m[e].size()){
-                    for(auto &x:m[e]){
-                        pos.push_back(x);
-                    }
-                    m[e].clear();
-                }
-                for(int x:pos){
-                    if(seen.find(x)==seen.end()){
-                        q.push(x);
-                        seen.insert(x);
-                    }
-                }
-            }
-            steps++;
-        }
-        return 0;
-    }
+	int minJumps(vector<int>& arr) {
+		int n = arr.size();
+		unordered_map<int, vector<int>>m;
+		for (int i = 0; i < n; i++) {
+			m[arr[i]].push_back(i);
+		}
+		queue<int>q;
+		q.push(0);
+		int steps = 0;
+		unordered_set<int>seen;
+		seen.insert(0);
+		while (q.size() > 0) {
+			int s = q.size();
+			for (int i = 0; i < s; i++) {
+				int v = q.front();
+				q.pop();
+				if (v == n - 1) {
+					return steps;
+				}
+				vector<int>pos;
+				if (v) {
+					pos.push_back(v - 1);
+				}
+				pos.push_back(v + 1);
+				int e = arr[v];
+				if (m[e].size()) {
+					for (auto &x : m[e]) {
+						pos.push_back(x);
+					}
+					m[e].clear();
+				}
+				for (int x : pos) {
+					if (seen.find(x) == seen.end()) {
+						q.push(x);
+						seen.insert(x);
+					}
+				}
+			}
+			steps++;
+		}
+		return 0;
+	}
 };
