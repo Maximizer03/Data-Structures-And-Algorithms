6d36bbe485d66bd20b2099308523bae488d546f3
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-05-09 13:13:04 +0530

    Time: 511 ms (50.60%), Space: 8 MB (74.88%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
-    bool exist(vector<vector<char>>& A, string s) {
-        int M = A.size(), N = A[0].size(), dirs[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
-        function<bool(int, int, int)> dfs = [&](int x, int y, int i) {
-            if (x < 0 || x >= M || y < 0 || y >= N || A[x][y] != s[i]) return false;
-            if (i + 1 == s.size()) return true;
-            char c = A[x][y];
-            A[x][y] = 0;
-            for (auto &[dx, dy] : dirs) {
-                if (dfs(x + dx, y + dy, i + 1)) return true;
-            }
-            A[x][y] = c;
-            return false;
-        };
-        for (int i = 0; i < M; ++i) {
-            for (int j = 0; j < N; ++j) {
-                if (dfs(i, j, 0)) return true;
-            }
-        }
-        return false;
-    }
+    vector<int>dx = {1, -1, 0, 0}, dy = {0, 0, 1, -1};
+    string w;
+	bool calc(int i, int j, int p, vector<vector<char>>& A, string &w) {
+		int n = A.size(), m = A[0].size();
+		if (i < 0 || j < 0 || i >= n || j >= m || A[i][j] != w[p]) {
+			return false;
+		}
+		if (p == w.size()-1) {
+			return true;
+		}
+		char c = A[i][j];
+		A[i][j] = '0';
+		for (int k = 0; k < 4; k++) {
+			if (calc(i + dx[k], j + dy[k], p + 1, A, w)) {
+				return true;
+			}
+		}
+		A[i][j] = c;
+		return false;
+	}
+	bool exist(vector<vector<char>>& A, string word) {
+        w=word;
+		int n = A.size(), m = A[0].size();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (calc(i, j, 0, A, w)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 };
