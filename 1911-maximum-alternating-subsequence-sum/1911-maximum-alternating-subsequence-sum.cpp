714aa7a8aeb5a236e49674df6c7e1d0d576f710d
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-05-10 23:31:30 +0530

    Time: 494 ms (11.46%), Space: 153.1 MB (17.09%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    long long dp[100005][2];
-    long long calc(int i, int p, vector<int>&nums){
-        if(i==nums.size()){
-            return 0;
-        }
-        if(dp[i][p]!=-1){
-            return dp[i][p];
-        }
-        long long ans= calc(i+1,p,nums);
-        long long x= nums[i];
-        if(p%2){
-            x*=-1;
-        }
-        ans=max(ans,calc(i+1,p^1,nums)+x);
-        return  dp[i][p]= ans;
-    }
-    long long maxAlternatingSum(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return calc(0,0,nums);
-    }
+	long long maxAlternatingSum(vector<int>& nums) {
+		int n = nums.size();
+		vector<vector<long long>>dp(n + 1, vector<long long>(2));
+		dp[n][0] = dp[n][1] = 0;
+		for (int i = n - 1; i >= 0; i--) {
+			for (int par = 0; par <= 1; par++) {
+				long long ans = dp[i + 1][par];
+				long long x = nums[i];
+				if (par % 2) {
+					x *= -1;
+				}
+				ans = max(ans, dp[i + 1][par ^ 1] + x);
+				dp[i][par] = ans;
+			}
+		}
+		return dp[0][0];
+	}
 };
