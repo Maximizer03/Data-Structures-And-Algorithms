21a13901cdc3f89117677d2121389987b7a06870
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-05-10 23:52:30 +0530

    Time: 623 ms (20.92%), Space: 11.1 MB (64.78%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -9,24 +9,25 @@ public:
 				cnt[i][(t[j] - '0')]++;
 			}
 		}
-		int dp[s + 1][n + 1][m + 1];
-		memset(dp, 0, sizeof(dp));
+		vector<vector<int>>prev(n + 1, vector<int>(m + 1));
+		vector<vector<int>>cur(n + 1, vector<int>(m + 1));
 		for (int i = 0; i <= n; i++) {
 			for (int j = 0; j <= m; j++) {
-				dp[s][i][j] = 1;
+				prev[i][j] = 1;
 			}
 		}
 		for (int i = s - 1; i >= 0; i--) {
 			for (int x = 0; x <= n; x++) {
 				for (int y = 0; y <= m; y++) {
-					int ans = dp[i + 1][x][y];
+					int ans = prev[x][y];
 					if (cnt[i][0] + x <= n && cnt[i][1] + y <= m) {
-						ans = max(ans,  dp[i + 1][cnt[i][0] + x][cnt[i][1] + y] + 1);
+						ans = max(ans,  prev[cnt[i][0] + x][cnt[i][1] + y] + 1);
 					}
-					dp[i][x][y] = ans;
+					cur[x][y] = ans;
 				}
 			}
+			swap(prev, cur);
 		}
-		return dp[0][0][0] - 1;
+		return prev[0][0] - 1;
 	}
 };
