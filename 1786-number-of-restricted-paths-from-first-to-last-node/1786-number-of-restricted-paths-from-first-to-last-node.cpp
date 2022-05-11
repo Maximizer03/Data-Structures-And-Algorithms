2883746b21ef6ea671c78b050accd5df51d55ba3
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-05-11 20:14:56 +0530

    Time: 898 ms (45.90%), Space: 160.9 MB (50.11%) - LeetHub

---
+++ 1786-number-of-restricted-paths-from-first-to-last-node/1786-number-of-restricted-paths-from-first-to-last-node.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+	int countRestrictedPaths(int n, vector<vector<int>>& edges) {
+		vector<vector<pair<long long , long long>>>g(n + 1);
+		for (int i = 0; i < edges.size(); i++) {
+			int u = edges[i][0], v = edges[i][1], w = edges[i][2];
+			g[u].push_back({v, w});
+			g[v].push_back({u, w});
+		}
+		int src = n;
+		priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>>q;
+		// (dist,src)
+		q.push({0, src});
+		vector<long long>dist(n + 1, 1e12);
+		dist[src] = 0;
+		while (!q.empty()) {
+			long long cost = q.top().first;
+			long long v = q.top().second;
+			q.pop();
+			if (cost > dist[v]) {
+				continue;
+			}
+			for (auto &[u, w] : g[v]) {
+				if (dist[v] + w < dist[u]) {
+					dist[u] = dist[v] + w;
+					q.push({dist[u], u});
+				}
+			}
+		}
+		long long mod = 1e9 + 7;
+		vector<long long>dp(n + 1, 0);
+		dp[n] = 1;
+		q.push({0, src});
+		vector<long long>dd(n + 1, 1e12);
+		dd[src] = 0;
+		while (!q.empty()) {
+			long long cost = q.top().first;
+			long long v = q.top().second;
+			q.pop();
+			if (cost > dd[v]) {
+				continue;
+			}
+			for (auto &[u, w] : g[v]) {
+				if (dd[v] + w < dd[u]) {
+					dd[u] = dd[v] + w;
+					q.push({dd[u], u});
+				}
+				if (dist[u] > dist[v]) {
+					dp[u] = (dp[u] % mod + dp[v] % mod) % mod;
+				}
+			}
+		}
+		return (int)dp[1];
+	}
+};
