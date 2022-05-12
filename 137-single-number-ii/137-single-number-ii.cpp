5cd833916ea2271feffeeb98ad36ac191635179a
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-05-12 21:22:37 +0530

    Time: 10 ms (65.48%), Space: 9.5 MB (56.14%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        vector<long long>cnt(33,0);
-        for(int x:nums){
-            x=abs(x);
-            for(int j=0;j<=32;j++){
-                long long bit=(1LL<<j)&x;
-                if(bit){
-                    cnt[j]++;
-                }
-            }
-        }
-        long long ans=0;
-        for(int i=0;i<33;i++){
-            if(cnt[i]%3==1){
-                ans= ans | (1LL<<i);
-            }
-        }
-        int v=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==ans){
-                v++;
-            }
-        }
-        if(v==1){
-            return ans;
-        }
-        else{
-            return -1*ans;
-        }
-    }
+	int singleNumber(vector<int>& nums) {
+		vector<int>cnt(32, 0);
+		for (int x : nums) {
+			x = abs(x);
+			for (int j = 0; j < 32; j++) {
+				int bit = (1LL << j)&x;
+				if (bit) {
+					cnt[j]++;
+				}
+			}
+		}
+		int ans = 0;
+		for (int i = 0; i < 32; i++) {
+			if (cnt[i] % 3 == 1) {
+				ans = ans | (1 << i);
+			}
+		}
+		int v = 0;
+		for (int i = 0; i < nums.size(); i++) {
+			if (nums[i] == ans) {
+				v++;
+			}
+		}
+		if (v == 1) {
+			return ans;
+		}
+		else {
+			return -1 * ans;
+		}
+	}
 };
