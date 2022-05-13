084ecf11c74a7002abb7429e498a73dda6cbe9d5
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-05-13 13:04:17 +0530

    Time: 2 ms (87.37%), Space: 6.2 MB (46.79%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    bool isValid(int row, int col, vector<string>&cur, int n) {
+	int cnt;
+	bool isValid(int row, int col, vector<string>&cur, int n) {
 		int r = row - 1, c = col - 1;
 		while (r >= 0 && c >= 0) {
 			if (cur[r][c] == 'Q') {
@@ -24,30 +25,25 @@ public:
 		}
 		return true;
 	}
-	void solve(int col, int n, vector<string>cur, vector<vector<string>>&res) {
+	void solve(int col, int n, vector<string>&cur) {
 		if (col == n) {
-			res.push_back(cur);
+			cnt++;
 			return;
 		}
 		for (int row = 0; row < n; row++) {
-			cur[row][col] = 'Q';
 			if (isValid(row, col, cur, n)) {
-				solve(col + 1, n, cur, res);
+				cur[row][col] = 'Q';
+				solve(col + 1, n, cur);
+				cur[row][col] = '.';
 			}
-			cur[row][col] = '.';
 		}
 	}
-    int totalNQueens(int n) {
-        vector<vector<string>>res;
-		vector<string>cur;
-		string t = "";
-		for (int i = 0; i < n; i++) {
-			t += '.';
-		}
-		for (int i = 0; i < n; i++) {
-			cur.push_back(t);
-		}
-		solve(0, n, cur, res);
-        return res.size();
-    }
+	int totalNQueens(int n) {
+		vector<vector<string>>res;
+		cnt = 0;
+		string t = string(n, '.');
+		vector<string>cur(n, t);
+		solve(0, n, cur);
+		return cnt;
+	}
 };
