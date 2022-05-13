ef22e6316db607361479239e12e557a02430eeb4
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-05-13 13:31:04 +0530

    Time: 7 ms (73.99%), Space: 7.3 MB (69.38%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,47 +1,26 @@
 class Solution {
 public:
-	bool isValid(int row, int col, vector<string>&cur, int n) {
-		int r = row - 1, c = col - 1;
-		while (r >= 0 && c >= 0) {
-			if (cur[r][c] == 'Q') {
-				return false;
-			}
-			r--; c--;
-		}
-		r = row, c = col - 1;
-		while (r >= 0 && c >= 0) {
-			if (cur[r][c] == 'Q') {
-				return false;
-			}
-			c--;
-		}
-		r = row + 1, c = col - 1;
-		while (r >= 0 && r < n && c >= 0) {
-			if (cur[r][c] == 'Q') {
-				return false;
-			}
-			r++; c--;
-		}
-		return true;
-	}
-	void solve(int col, int n, vector<string>&cur, vector<vector<string>>&res) {
+	void solve(int col, int n, vector<string>&cur, vector<vector<string>>&res, vector<int>&LR, vector<int>&LD, vector<int>&UD) {
 		if (col == n) {
 			res.push_back(cur);
 			return;
 		}
 		for (int row = 0; row < n; row++) {
-			if (isValid(row, col, cur, n)) {
-                cur[row][col] = 'Q';
-				solve(col + 1, n, cur, res);
-                cur[row][col] = '.';
+			if (LR[row] == 0 && LD[row + col] == 0 && UD[n - 1 + col - row] == 0) {
+				cur[row][col] = 'Q';
+				LR[row] = 1 ,LD[row + col] = 1 ,UD[n - 1 + col - row] = 1;
+				solve(col + 1, n, cur, res, LR, LD, UD);
+				LR[row] = 0 ,LD[row + col] = 0 ,UD[n - 1 + col - row] = 0;
+				cur[row][col] = '.';
 			}
 		}
 	}
 	vector<vector<string>> solveNQueens(int n) {
 		vector<vector<string>>res;
-        string t = string(n,'.');
-		vector<string>cur(n,t);
-		solve(0, n, cur, res);
+		string t = string(n, '.');
+		vector<string>cur(n, t);
+		vector<int>LR(n, 0), LD(2 * n, 0), UD(2 * n, 0);
+		solve(0, n, cur, res, LR, LD, UD);
 		return res;
 	}
 };
