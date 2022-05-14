ef888d80bf61fe667248acc10b8f7b0853332784
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-05-14 14:30:09 +0530

    Time: 119 ms (61.54%), Space: 42.3 MB (64.15%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cpp
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cpp
@@ -2,29 +2,34 @@ class Solution {
 public:
 	int sumSubarrayMins(vector<int>& a) {
 		int n = a.size();
-		vector<int>L(n), R(n);
-		stack<pair<int, int>>st;
-		for (int i = 0; i < n; i++) {
-			while (!st.empty() && st.top().first > a[i]) {
-				st.pop();
+		vector<int>ngr(n, n), ngl(n, -1), st;
+		for (int i = n - 1; i >= 0; i--) {
+			while (st.size() > 0 && a[st.back()] > a[i]) {
+				st.pop_back();
 			}
-			L[i] = st.empty() ? i + 1 : i - st.top().second;
-			st.push({a[i], i});
+			if (!st.empty()) {
+				ngr[i] = st.back();
+			}
+			st.push_back(i);
 		}
 		while (!st.empty()) {
-			st.pop();
+			st.pop_back();
 		}
-		for (int i = n - 1; i >= 0; i--) {
-			while (!st.empty() && st.top().first >= a[i]) {
-				st.pop();
-			}
-			R[i] = st.empty() ? n - i : st.top().second - i;
-			st.push({a[i], i});
-		}
-		int ans = 0, mod = 1e9 + 7;
 		for (int i = 0; i < n; i++) {
-			long long p = (long long)(L[i]) * R[i] * (a[i]);
-			ans = (ans % mod + p % mod) % mod;
+			while (!st.empty() && a[st.back()] >= a[i]) {
+				st.pop_back();
+			}
+			if (!st.empty()) {
+				ngl[i] = st.back();
+			}
+			st.push_back(i);
+		}
+		long long ans = 0,mod=1e9+7;
+		for (int i = 0; i < n; i++) {
+            long long alpha= (i - ngl[i])%mod;
+            long long beta= (ngr[i] - i)%mod;
+			long long t = (alpha*beta)%mod;
+			ans= (ans%mod + t%mod*a[i]%mod)%mod;
 		}
 		return ans;
 	}
