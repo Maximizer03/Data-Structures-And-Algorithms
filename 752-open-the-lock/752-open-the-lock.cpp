9e55164455665c12162e84d29ac27351441c2d74
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-05-22 16:16:21 +0530

    Time: 266 ms (62.23%), Space: 34.7 MB (74.89%) - LeetHub

--- 752-open-the-lock/752-open-the-lock.cpp
+++ 752-open-the-lock/752-open-the-lock.cpp
@@ -8,11 +8,11 @@ public:
 		}
 		int steps = 0;
 		queue<string>q;
-        if(s.find(cur)!=s.end()){
-            return -1;
-        }
+		if (s.find(cur) != s.end()) {
+			return -1;
+		}
 		q.push(cur);
-        s.insert(cur);
+		s.insert(cur);
 		while (!q.empty()) {
 			int si = q.size();
 			while (si--) {
@@ -22,22 +22,23 @@ public:
 					return steps;
 				}
 				for (int i = 0; i < 4; i++) {
-					string t = v;
-					int d = t[i] - '0';
+                    char org= v[i];
+					int d = v[i] - '0';
 					int nxt = (d + 1) % 10;
 					int prev = (d - 1 + 10) % 10;
 					char nn = nxt + '0';
 					char pp = prev + '0';
-					t[i] = nn;
-					if (s.find(t) == s.end()) {
-						q.push(t);
-						s.insert(t);
+					v[i] = nn;
+					if (s.find(v) == s.end()) {
+						q.push(v);
+						s.insert(v);
 					}
-					t[i] = pp;
-					if (s.find(t) == s.end()) {
-						q.push(t);
-						s.insert(t);
+					v[i] = pp;
+					if (s.find(v) == s.end()) {
+						q.push(v);
+						s.insert(v);
 					}
+                    v[i]=org;
 				}
 			}
 			steps++;
