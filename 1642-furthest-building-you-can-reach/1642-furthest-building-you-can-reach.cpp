94f94d7311b1163f8201618703bb3dde09aa1b8f
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-05-24 15:19:55 +0530

    Time: 183 ms (22.35%), Space: 53.6 MB (78.60%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,41 +1,22 @@
 class Solution {
 public:
-	int calc(int m, vector<int>&h, int B, int L) {
-		vector<int>v;
-		for (int i = 1; i <= m; i++) {
-			if (h[i] > h[i - 1]) {
-				v.push_back(h[i] - h[i - 1]);
-			}
-		}
-		sort(begin(v), end(v));
-		int s = v.size();
-		while (v.size() > 0 && L > 0) {
-			L--;
-			v.pop_back();
-		}
-		if (v.size() == 0) {
-			return 1;
-		}
-		long long sum = 0;
-		for (int i = 0; i < v.size(); i++) {
-			sum += v[i];
-		}
-		return (long long)B >= sum;
-
-	}
 	int furthestBuilding(vector<int>& h, int B, int L) {
 		int ans = 0;
-        int n=h.size();
-		int l = 1, r = n - 1;
-		while (l <= r) {
-			int m = (l + r) / 2;
-			if (calc(m, h, B, L)) {
-				ans = m;
-				l = m + 1;
+		int n = h.size();
+		priority_queue<int, vector<int>, greater<int>>q;
+		long long sum = 0;
+		for (int i = 1; i < n; i++) {
+			if (h[i] > h[i - 1]) {
+				q.push(h[i] - h[i - 1]);
 			}
-			else {
-				r = m - 1;
+			if (q.size() > L) {
+				sum += q.top();
+				q.pop();
+				if (sum > B) {
+					return ans;
+				}
 			}
+			ans = i;
 		}
 		return ans;
 	}
