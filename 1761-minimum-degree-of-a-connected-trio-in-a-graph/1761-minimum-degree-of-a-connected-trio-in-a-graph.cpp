0f8107396b4174f462a48d0145816978808126f1
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-05-26 12:26:31 +0530

    Time: 527 ms (59.15%), Space: 46.6 MB (31.06%) - LeetHub

--- 1761-minimum-degree-of-a-connected-trio-in-a-graph/1761-minimum-degree-of-a-connected-trio-in-a-graph.cpp
+++ 1761-minimum-degree-of-a-connected-trio-in-a-graph/1761-minimum-degree-of-a-connected-trio-in-a-graph.cpp
@@ -3,12 +3,12 @@ public:
 	int minTrioDegree(int n, vector<vector<int>>& e) {
 		vector<vector<int>>g(n);
 		vector<int>deg(n, 0);
-        vector<vector<int>>mat(n, vector<int>(n, 0));
+		vector<vector<int>>mat(n, vector<int>(n, 0));
 		for (int i = 0; i < e.size(); i++) {
 			int u = e[i][0] - 1, v = e[i][1] - 1;
 			g[u].push_back(v);
 			g[v].push_back(u);
-            mat[u][v] = 1; mat[v][u] = 1;
+			mat[u][v] = 1; mat[v][u] = 1;
 			deg[u]++; deg[v]++;
 		}
 		int ans = 1e9;
@@ -26,9 +26,9 @@ public:
 				}
 			}
 		}
-        if(ans==1e9){
-            return -1;
-        }
+		if (ans == 1e9) {
+			return -1;
+		}
 		return ans;
 	}
 };
