947a95813db33be5ed1a3718637784e16d1bc3fd
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-05-26 12:26:03 +0530

    Time: 550 ms (55.32%), Space: 46.6 MB (31.06%) - LeetHub

--- 1761-minimum-degree-of-a-connected-trio-in-a-graph/1761-minimum-degree-of-a-connected-trio-in-a-graph.cpp
+++ 1761-minimum-degree-of-a-connected-trio-in-a-graph/1761-minimum-degree-of-a-connected-trio-in-a-graph.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
 	int minTrioDegree(int n, vector<vector<int>>& e) {
+		vector<vector<int>>g(n);
 		vector<int>deg(n, 0);
-		vector<vector<int>>mat(n, vector<int>(n, 0));
+        vector<vector<int>>mat(n, vector<int>(n, 0));
 		for (int i = 0; i < e.size(); i++) {
 			int u = e[i][0] - 1, v = e[i][1] - 1;
-			mat[u][v] = 1;
-			mat[v][u] = 1;
+			g[u].push_back(v);
+			g[v].push_back(u);
+            mat[u][v] = 1; mat[v][u] = 1;
 			deg[u]++; deg[v]++;
 		}
 		int ans = 1e9;
 		for (int i = 0; i < n; i++) {
-			for (int j = i + 1; j < n; j++) {
-				for (int k = j + 1; k < n; k++) {
-					if (mat[i][j] && mat[i][k] && mat[k][j]) {
-						ans = min(ans, deg[i] + deg[j] + deg[k] - 6);
+			for (int j : g[i]) {
+				if (j > i) {
+					for (int k : g[j]) {
+						if (k > j) {
+							if (mat[i][k]) {
+								ans = min(ans, deg[i] + deg[j] + deg[k] - 6);
+							}
+
+						}
 					}
 				}
 			}
 		}
-		if (ans == 1e9) {
-			return -1;
-		}
+        if(ans==1e9){
+            return -1;
+        }
 		return ans;
 	}
 };
