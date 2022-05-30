0622ff62329126dac892942f567dab6286ef0e86
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-05-30 23:12:18 +0530

    Time: 683 ms (5.43%), Space: 7.5 MB (43.44%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -3,16 +3,16 @@ public:
 	int ans = 0;
 	vector<int>dx, dy;
 	int n, m;
-	void dfs(int i, int j, vector<vector<int>>&grid, vector<vector<int>>&vis, int cost) {
+	void dfs(int i, int j, vector<vector<int>>&grid, int cost) {
 		ans = max(ans, cost);
 		for (int k = 0; k < 4; k++) {
 			int nx = i + dx[k], ny = j + dy[k];
-			if (nx >= 0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] > 0 && vis[nx][ny] == 0) {
-				vis[nx][ny] = 1;
+			if (nx >= 0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] > 0) {
 				cost += grid[nx][ny];
-				dfs(nx, ny, grid, vis, cost);
+				grid[nx][ny] *= -1;
+				dfs(nx, ny, grid, cost);
+				grid[nx][ny] *= -1;
 				cost -= grid[nx][ny];
-				vis[nx][ny] = 0;
 			}
 		}
 	}
@@ -20,13 +20,13 @@ public:
 		n = grid.size(), m = grid[0].size();
 		dx = {1, -1, 0, 0};
 		dy = {0, 0, 1, -1};
-		vector<vector<int>>vis(n, vector<int>(m));
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				if (grid[i][j]) {
-					vis[i][j] = 1;
-					dfs(i, j, grid, vis, grid[i][j]);
-					vis[i][j] = 0;
+					int l = grid[i][j];
+					grid[i][j] *= -1;
+					dfs(i, j, grid, l);
+					grid[i][j] *= -1;
 				}
 			}
 		}
