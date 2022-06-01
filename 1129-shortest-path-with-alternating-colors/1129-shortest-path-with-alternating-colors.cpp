ad671518a11e8d998c9d15886ed844af3f3c0d15
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-06-01 15:56:42 +0530

    Time: 42 ms (16.70%), Space: 14.8 MB (72.20%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 	vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
 		queue<pair<int, int>>q;
-		vector<vector<int>>dp(n, vector<int>(2, 1e9));
+		vector<vector<int>>dp(n, vector<int>(3, 1e9));
 		vector<vector<pair<int, int>>>g(n);
 		// 0 for red and 1 for blue
 		for (auto &x : redEdges) {
@@ -13,11 +13,7 @@ public:
 			int u = x[0], v = x[1];
 			g[u].push_back({v, 1});
 		}
-		set<pair<int, int>>vis;
-		dp[0][0] = dp[0][1] = 0;
-        vis.insert({0,1});
-        vis.insert({0,0});
-		vis.insert({0, -1});
+		dp[0][0] = dp[0][1] = dp[0][2]=0;
 		q.push({0, -1});
 		int steps = 0;
 		while (!q.empty()) {
@@ -26,35 +22,20 @@ public:
 				int v = q.front().first;
 				int col = q.front().second;
 				q.pop();
-				if (col == -1) {
-					for (auto &[u, c] : g[v]) {
-						if (vis.find({u, c}) == vis.end()) {
-							vis.insert({u, c});
-							dp[u][c] = steps + 1;
-							q.push({u, c});
-						}
-					}
-				}
-				else {
-					for (auto &[u, c] : g[v]) {
-						if (c != col && vis.find({u, c}) == vis.end()) {
-							vis.insert({u, c});
-							dp[u][c] = steps + 1;
-							q.push({u, c});
-						}
+				for (auto &[u, c] : g[v]) {
+					if (c != col && steps+1 < dp[u][c]) {
+						dp[u][c] = steps + 1;
+						q.push({u, c});
 					}
 				}
 			}
 			steps++;
 		}
-		vector<int>ans;
+		vector<int>ans(n,-1);
 		for (int i = 0; i < n; i++) {
 			int x = min(dp[i][0], dp[i][1]);
 			if (x != 1e9) {
-				ans.push_back(x);
-			}
-			else {
-				ans.push_back(-1);
+				ans[i]=x;
 			}
 		}
 		return ans;
