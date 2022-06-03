ba2a3a08b60f7dcb173b763fdc64c8bc91b79244
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-06-03 19:01:38 +0530

    Time: 2 ms (82.97%), Space: 6 MB (71.08%) - LeetHub

--- 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
 	int maximumScore(int a, int b, int c) {
-		priority_queue<int>q;
-		q.push(a); q.push(b); q.push(c);
-		int ans = 0;
-		while (q.size() >= 2) {
-			int x = q.top();
-			q.pop();
-			int y = q.top();
-			q.pop();
-			ans++;
-			x--; y--;
-			if (x > 0) {
-				q.push(x);
-			}
-			if (y > 0) {
-				q.push(y);
-			}
-		}
-		return ans;
+		vector<int>arr;
+        arr.push_back(a);arr.push_back(b);
+        arr.push_back(c);
+        sort(begin(arr),end(arr));
+        if(arr[2]> arr[1]+arr[0]){
+            return arr[1]+arr[0];
+        }
+        return (arr[0]+arr[1]+arr[2])/2;
 	}
 };
