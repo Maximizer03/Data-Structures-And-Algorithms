c2985f6e1c8d41417e20d4177b58ff6957688e18
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-06-07 03:47:57 +0530

    Time: 165 ms (57.56%), Space: 7.5 MB (59.88%) - LeetHub

--- 1617-count-subtrees-with-max-distance-between-cities/1617-count-subtrees-with-max-distance-between-cities.cpp
+++ 1617-count-subtrees-with-max-distance-between-cities/1617-count-subtrees-with-max-distance-between-cities.cpp
@@ -1,59 +1,59 @@
 class Solution {
 public:
-     vector<int> countSubgraphsForEachDiameter(int n, vector<vector<int>>& edges) {
-          vector<int>d(n + 1, 0);
-          int INF = 1e5;
-          vector<vector<int>>dp(n + 1, vector<int>(n + 1, INF));
-          for (int v = 0; v < n; v++) {
-               dp[v][v] = 0;
-          }
-          for (auto e : edges) {
-               dp[e[0] - 1][e[1] - 1] = 1;
-               dp[e[1] - 1][e[0] - 1] = 1;
-          }
-          for (int k = 0; k < n; ++k) {
-               for (int i = 0; i < n; ++i) {
-                    for (int j = 0; j < n; ++j) {
-                         if (dp[i][k] < INF && dp[k][j] < INF) {
-                              dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
-                         }
+    vector<int> get(int src, vector<vector<int>>&g, int n) {
+        queue<int>q;
+        vector<int>dist(n), vis(n);
+        q.push(src);
+        vis[src] = 1;
+        int steps = 0;
+        while (!q.empty()) {
+            int s = q.size();
+            while (s--) {
+                int v = q.front();
+                q.pop();
+                dist[v] = steps;
+                for (auto &u : g[v]) {
+                    if (!vis[u]) {
+                        q.push(u);
+                        vis[u] = 1;
                     }
-               }
-          }
-          for (int mask = 1; mask < (1 << n); mask++) {
-               vector<int>node;
-               for (int v = 0; v < n; v++) {
-                    int bit = (1 << v)&mask;
-                    if (bit > 0) {
-                         node.push_back(v);
+                }
+            }
+            steps++;
+        }
+        return dist;
+    }
+    vector<int> countSubgraphsForEachDiameter(int n, vector<vector<int>>& e) {
+        vector<vector<int>>g(n);
+        for (int i = 0; i < e.size(); i++) {
+            g[e[i][0] - 1].push_back(e[i][1] - 1);
+            g[e[i][1] - 1].push_back(e[i][0] - 1);
+        }
+        vector<vector<int>>dp(n);
+        for (int i = 0; i < n; i++) {
+            dp[i] = get(i, g, n);
+        }
+        vector<int>ans(n - 1);
+        for (int m = 1; m < (1 << n); m++) {
+            int tot = __builtin_popcount(m), cnt = 0;
+            int mx = 0;
+            for (int j = 0; j < n; j++) {
+                if ((1 << j)&m) {
+                    for (int k = 0; k < n && k != j; k++) {
+                        if ((1 << k)&m) {
+                            // direct edge
+                            if (dp[j][k] == 1) {
+                                cnt++;
+                            }
+                            mx = max(mx, dp[j][k]);
+                        }
                     }
-               }
-               int cnt = 0;
-               for (int i = 0; i < n; i++) {
-                    for (int j = i + 1; j < n; j++) {
-                         int b1 = (1 << i)&mask;
-                         int b2 = (1 << j)&mask;
-                         if (b1 > 0 && b2 > 0) {
-                              if (dp[i][j] == 1) {
-                                   cnt++;
-                              }
-                         }
-                    }
-               }
-               if (cnt == node.size() - 1) {
-                    int mx = 0;
-                    for (auto x : node) {
-                         for (auto y : node) {
-                              mx = max(mx, dp[x][y]);
-                         }
-                    }
-                    d[mx]++;
-               }
-          }
-          vector<int>ans;
-          for (int i = 1; i <= n - 1; i++) {
-               ans.push_back(d[i]);
-          }
-          return ans;
-     }
+                }
+            }
+            if (cnt == tot - 1 && mx > 0) {
+                ans[mx - 1]++;
+            }
+        }
+        return ans;
+    }
 };
