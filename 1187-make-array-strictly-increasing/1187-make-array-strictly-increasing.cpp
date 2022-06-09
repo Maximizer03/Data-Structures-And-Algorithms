e3436db0b42b0f773eca4b688974df1d8edaf24c
Yuvraj Neema <97230497+Maximizer03@users.noreply.github.com>
2022-06-09 13:35:40 +0530

    Time: 1524 ms (8.17%), Space: 43.3 MB (37.74%) - LeetHub

--- 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
+++ 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
@@ -1,49 +1,52 @@
 class Solution {
 public:
-    int dp[2005][2][2005];
-    int calc(int i, int c, int p, vector<int>&arr1, vector<int>&arr2) {
-        int n = arr1.size(), m = arr2.size();
-        if (i == n) {
-            return 0;
-        }
-        if (dp[i][c][p] != -1) {
-            return dp[i][c][p];
-        }
-        int ans = 1e9;
-        if (i == 0) {
-            ans = calc(i + 1, 0, 0, arr1, arr2);
-            ans = min(ans, 1 + calc(i + 1, 1, 0, arr1, arr2));
-        }
-        else {
-            if (c == 1) {
-                // not change current
-                if (arr1[i] > arr2[p]) {
-                    ans = min(ans, calc(i + 1, 0, 0, arr1, arr2));
-                }
-                // change currrent
-                int j = upper_bound(begin(arr2), end(arr2), arr2[p]) - arr2.begin();
-                if (j < m) {
-                    ans = min(ans, calc(i + 1, 1, j, arr1, arr2) + 1);
-                }
-            }
-            else {
-                // not change current
-                if (arr1[i] > arr1[i - 1]) {
-                    ans = min(ans, calc(i + 1, 0, 0, arr1, arr2));
-                }
-                // change currrent
-                int j = upper_bound(begin(arr2), end(arr2), arr1[i - 1]) - arr2.begin();
-                if (j < m) {
-                    ans = min(ans, calc(i + 1, 1, j, arr1, arr2) + 1);
-                }
-            }
-        }
-        return dp[i][c][p] = ans;
-    }
     int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
-        memset(dp, -1, sizeof(dp));
+        int n = arr1.size(), m = arr2.size();
         sort(begin(arr2), end(arr2));
-        int ans = calc(0, 0, 0, arr1, arr2);
+        int dp[n + 1][2][m];
+        memset(dp, 1e9, sizeof(dp));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < 2; j++) {
+                dp[n][j][i] = 0;
+            }
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int c = 0; c < 2; c++) {
+                for (int p = 0; p < m; p++) {
+                    int ans = 1e9;
+                    if (i == 0) {
+                        ans = dp[i + 1][0][0];
+                        ans = min(ans, 1 + dp[i + 1][1][0]);
+                    }
+                    else {
+                        if (c == 1) {
+                            // not change current
+                            if (arr1[i] > arr2[p]) {
+                                ans = min(ans, dp[i + 1][0][0]);
+                            }
+                            // change currrent
+                            int j = upper_bound(begin(arr2), end(arr2), arr2[p]) - arr2.begin();
+                            if (j < m) {
+                                ans = min(ans, dp[i + 1][1][j] + 1);
+                            }
+                        }
+                        else {
+                            // not change current
+                            if (arr1[i] > arr1[i - 1]) {
+                                ans = min(ans, dp[i + 1][0][0]);
+                            }
+                            // change currrent
+                            int j = upper_bound(begin(arr2), end(arr2), arr1[i - 1]) - arr2.begin();
+                            if (j < m) {
+                                ans = min(ans, dp[i + 1][1][j] + 1);
+                            }
+                        }
+                    }
+                    dp[i][c][p] = ans;
+                }
+            }
+        }
+        int ans = dp[0][0][0];
         if (ans == 1e9) {
             return -1;
         }
